41825ed9310a7ff2835d2bc96e12441d3d37361b
Saul Nuñez <saul.nunez99@gmail.com>
2020-09-06 19:07:30 -0600

    Arreglar pruebas para las otras opciones disponibles en el check de sintaxis

--- __tests__/puzzle_check.test.js
+++ __tests__/puzzle_check.test.js
@@ -1,12 +1,12 @@
-const GetCodeOutput = require('../src/CheckPuzzle/index.js');
-const CheckSyntax = require('../src/CheckPuzzle/index.js');
+import { GetCodeOutput, CheckSyntax } from '../src/CheckPuzzle/index.js';
+
 const fs = require('fs');
 const path = require('path');
 
 describe('testing code output tests', () => {
     test('returns basic stuff', () => {
         const testCode = "alert('hello_world')";
-        expect(GetCodeOutput(testCode)).toBe("hello_world");
+        expect(GetCodeOutput(testCode)).toBe(["hello_world"]);
     })
 });
 
@@ -14,5 +14,15 @@ describe('testing syntax analysis result', () => {
     test('returns correctly on conditional', () => {
         const testCodeConditional = fs.readFileSync(path.join(__dirname, './conditionals.txt'),{ encoding: 'utf8' });
         expect(CheckSyntax(testCodeConditional, 'check_for_branching')).toBe(true);
-    })
+    });
+
+    test('returns correctly on cycles', () => {
+        const testCodeConditional = fs.readFileSync(path.join(__dirname, './cycles.txt'),{ encoding: 'utf8' });
+        expect(CheckSyntax(testCodeConditional, 'check_for_loops')).toBe(true);
+    });
+
+    test('returns correctly on functions', () => {
+        const testCodeConditional = fs.readFileSync(path.join(__dirname, './functions.txt'),{ encoding: 'utf8' });
+        expect(CheckSyntax(testCodeConditional, 'check_for_functions')).toBe(true);
+    });
 });
